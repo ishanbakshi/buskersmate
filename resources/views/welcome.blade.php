e0adbce7cdf628cf59ef63aa2e9feeb537115cb0
ishan bakshi <ishan.bakshi@gmail.com>
2015-02-12 00:32:19 +1100

    first commit trying again

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+
+
+<html>
+	<head>
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: table;
+				font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				margin-bottom: 40px;
+			}
+
+			.quote {
+        font-size: 24px;
+        padding : 0 60 20 60;
+      }
+      .enter-link{
+        padding: 10px 15px;
+        background: #4479BA;
+        color: #FFF;
+        margin-top: 40px;
+        border-radius: 4px;
+        border: solid 1px #20538D;
+        text-decoration: none;
+      }
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">Buskers Mate</div>
+        <div class="quote">CD's are soon going to be extinct like the cassettes and the Record Players. But we are not going to let the busker community at the hands of some greedy music streaming companies. There is a big difference between buying music online and picking up a CD from ur favourite busker on the free streets. We want to keep things the same, but yet empower the musicians with technology.</div>
+        <div>
+          <a href="/home" class = "enter-link">Enter</a>
+        </div>
+			</div>
+		</div>
+	</body>
+</html>
